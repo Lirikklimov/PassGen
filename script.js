e1e1bf833c0bec7a83003861f2b4a3144f650f00
Kirill Klimov <lirikklimov@gmail.com>
2023-01-27 16:21:52 +0400

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function getPassword(){
+    let chars = "01234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()-_=+[{}]<>,.?/";
+    let passwordLength = 16;
+    let password = "";
+
+    for(let i=0; i<passwordLength; i++) {
+        let randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById("password").value = password;
+}
+getPassword();
