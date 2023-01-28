e47f090851c5e98e604d64d3cef5d647c847f3b0
Kirill Klimov <lirikklimov@gmail.com>
2023-01-28 16:41:39 +0400

    Added Copy Text

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function getPassword(){
 }
 getPassword();
 
+document.getElementsByClassName('password').addEventListener('click', async event => {
+    if (!navigator.clipboard) {
+        // Clipboard API not available
+        return
+      }
+      const text = event.target.innerText
+      try {
+        await navigator.clipboard.writeText(text)
+        event.target.textContent = 'Copied to clipboard'
+      } catch (err) {
+        console.error('Failed to copy!', err)
+      }
+})
